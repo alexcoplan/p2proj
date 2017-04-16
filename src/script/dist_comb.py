41912a65198fbc1babc6cc9b481383685f38094b
Alex Coplan <lexy0202@gmail.com>
2017-04-16 20:50:04 +0100

    nearly done on MVS exposition

--- src/script/dist_comb.py
+++ src/script/dist_comb.py
@@ -39,6 +39,7 @@ args = parser.parse_args()
 flat_dist = [0.25]*4
 skew_dist = [0.5,0.25,1.0/8.0, 1.0/8.0]
 peak_dist = [3.0/4.0, 1.0/8.0, 1.0/16.0, 1.0/16.0]
+vpeak_dist = [3.0/4.0, 3.0/16.0, 1.0/32.0, 1.0/32.0]
 
 distributions = {
   "both_flat" : [flat_dist]*2,
@@ -80,24 +81,57 @@ if args.gen_tests:
     outfile.write(json.dumps(root_obj, indent=2))
 else: 
   # if we're not generating tests, then we'll just generate an example plot
-  dist_x, dist_y = peak_dist, list(reversed(peak_dist))
-  dist_size = len(dist_x)
-  assert dist_size == len(dist_y)
-  arith, geom = combine_dists([dist_x, dist_y], 6.0)
-  bar_width = 0.2
+  dist_a, dist_b = peak_dist, list(reversed(peak_dist))
+  dist_c, dist_d = flat_dist, vpeak_dist
+  dist_size = len(dist_a)
+  assert dist_size == len(dist_b) == len(dist_c) == len(dist_d)
+  arith_ab, geom_ab = combine_dists([dist_a, dist_b], 2.0)
+  arith_cd, geom_cd = combine_dists([dist_c, dist_d], 2.0)
+  bar_width = 0.4
   bar_idxs = np.arange(dist_size)
 
-  fig, ax = plt.subplots()
-  distx_bars = ax.bar(bar_idxs, dist_x, bar_width, color='b')
-  disty_bars = ax.bar(bar_idxs + bar_width, dist_y, bar_width, color='g')
-  arith_bars = ax.bar(bar_idxs + bar_width*2, arith, bar_width, color='c')
-  geom_bars  = ax.bar(bar_idxs + bar_width*3, geom, bar_width, color='m')
+  fig = plt.figure(figsize=(7,7))
+  
+  ax_a  = fig.add_subplot(321)
+  ax_b  = fig.add_subplot(323)
+  ax_ab = fig.add_subplot(325)
 
-  ax.set_ylabel('Probability')
-  ax.set_title('Comparison of Geometric and Arithmetic Distribution Combination')
-  ax.legend(
-    (distx_bars[0], disty_bars[0], arith_bars[0], geom_bars[0]), 
-    ('Distribution A', 'Distribution B', 'Arithmetic', 'Geometric')
+  ax_c  = fig.add_subplot(322)
+  ax_d  = fig.add_subplot(324)
+  ax_cd = fig.add_subplot(326)
+
+  dista_bars = ax_a.bar(bar_idxs, dist_a, bar_width, color='r')
+  distb_bars = ax_b.bar(bar_idxs, dist_b, bar_width, color='r')
+  distc_bars = ax_c.bar(bar_idxs, dist_c, bar_width, color='r')
+  distd_bars = ax_d.bar(bar_idxs, dist_d, bar_width, color='r')
+
+  arith_ab_bars = ax_ab.bar(bar_idxs, arith_ab, bar_width, color='b')
+  geom_ab_bars  = ax_ab.bar(bar_idxs + bar_width, geom_ab, bar_width, color='g')
+  
+  arith_cd_bars = ax_cd.bar(bar_idxs, arith_cd, bar_width, color='b')
+  geom_cd_bars  = ax_cd.bar(bar_idxs + bar_width, geom_cd, bar_width, color='g')
+
+  for axes in [ax_a, ax_b, ax_ab, ax_c, ax_d, ax_cd]:
+    axes.set_ylabel('Probability')
+    axes.set_ylim([0.0,1.0])
+
+  ax_a.set_title('Distribution A')
+  ax_b.set_title('Distribution B')
+  ax_c.set_title('Distribution C')
+  ax_d.set_title('Distribution D')
+  ax_ab.set_title('Combination of A and B')
+  ax_cd.set_title('Combination of C and D')
+
+  ax_ab.legend(
+    (arith_ab_bars[0], geom_ab_bars[0]), 
+    ('Arithmetic', 'Geometric')
+  )
+  ax_cd.legend(
+    (arith_cd_bars[0], geom_cd_bars[0]), 
+    ('Arithmetic', 'Geometric')
   )
 
-  plt.show()
+  plt.tight_layout()
+  plt.subplots_adjust(bottom=0.1)
+
+  plt.savefig('comb.svg')
